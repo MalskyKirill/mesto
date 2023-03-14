3258c4d2a3da1dc70914af3b78bf9567d8afe05a
Kirill Malskiy <sabbat@mail.ru>
2023-03-14 23:40:58 +0300

    правки после первого ревью

--- scripts/index.js
+++ scripts/index.js
@@ -2,29 +2,26 @@ const openPopupBtnElement = document.querySelector('.profile__edit-button');
 const popupElement = document.querySelector('.popup');
 const closePopupBtnElement = document.querySelector('.popup__close');
 const formElement = document.querySelector('.popup__form');
-const nameFormFieldElement = formElement.querySelector('.popup__field_name');
-const jobFormFieldElement = formElement.querySelector('.popup__field_job');
+const nameFormFieldElement = formElement.querySelector('.popup__field_next_name');
+const jobFormFieldElement = formElement.querySelector('.popup__field_next_job');
 const profileName = document.querySelector('.profile__name');
 const profileJob = document.querySelector('.profile__job');
 
-
 // открытие-закрытие попапа
 function openPopup() {
-  popupElement.classList.add('popup_active');
+  popupElement.classList.add('popup_opened');
+  nameFormFieldElement.value = profileName.textContent;
+  jobFormFieldElement.value = profileJob.textContent;
 }
 
 function closePopup() {
-  popupElement.classList.remove('popup_active');
+  popupElement.classList.remove('popup_opened');
 }
 
-function closePopupByClickOnOverlay(evt) {
-  if (evt.target !== evt.currentTarget) return;
-  closePopup();
-}
-
-openPopupBtnElement.addEventListener('click', openPopup);
-closePopupBtnElement.addEventListener('click', closePopup);
-popupElement.addEventListener('click', closePopupByClickOnOverlay);
+// function closePopupByClickOnOverlay(evt) {
+//   if (evt.target !== evt.currentTarget) return;
+//   closePopup();
+// }
 
 // редактирование формы при сохранении
 function handleFormSubmit(evt) {
@@ -35,4 +32,7 @@ function handleFormSubmit(evt) {
   closePopup();
 }
 
+openPopupBtnElement.addEventListener('click', openPopup);
+closePopupBtnElement.addEventListener('click', closePopup);
+// popupElement.addEventListener('click', closePopupByClickOnOverlay);
 formElement.addEventListener('submit', handleFormSubmit);
