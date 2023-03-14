450e34955af08bad35f04117d90f01b5f9f0a8be
Kirill Malskiy <sabbat@mail.ru>
2023-03-14 15:36:35 +0300

    реализовал редактирование имени и информации о себе, отредактировал README

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
 const openPopupBtnElement = document.querySelector('.profile__edit-button');
 const popupElement = document.querySelector('.popup');
 const closePopupBtnElement = document.querySelector('.popup__close');
+const formElement = document.querySelector('.popup__form');
+const nameFormFieldElement = formElement.querySelector('.popup__field_name');
+const jobFormFieldElement = formElement.querySelector('.popup__field_job');
+const profileName = document.querySelector('.profile__name');
+const profileJob = document.querySelector('.profile__job');
 
 
 // открытие-закрытие попапа
-function togglePopupVisibility() {
-  popupElement.classList.toggle('popup_active');
-}
-
 function openPopup() {
   popupElement.classList.add('popup_active');
 }
@@ -26,3 +27,12 @@ closePopupBtnElement.addEventListener('click', closePopup);
 popupElement.addEventListener('click', closePopupByClickOnOverlay);
 
 // редактирование формы при сохранении
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+
+  profileName.textContent = nameFormFieldElement.value;
+  profileJob.textContent = jobFormFieldElement.value;
+  closePopup();
+}
+
+formElement.addEventListener('submit', handleFormSubmit);
